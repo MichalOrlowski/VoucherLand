52458ba608d44543f8875792d46e094da6d6e626
mior <michal.orlowski@jcommerce.pl>
2016-02-12 22:56:28 +0100

    Added voucher validator.

--- public/js/controllers/mainController.js
+++ public/js/controllers/mainController.js
@@ -1,30 +1,35 @@
 angular.module('voucherController', [])
 
-	// inject the Todo service factory into our controller
-	.controller('mainController', function($scope, $http, Vouchers) {
-		$scope.formData = {};
+    // inject the Todo service factory into our controller
+    .controller('mainController', function ($scope, $http, Vouchers) {
+        $scope.formData = {};
+        $scope.errorMessage;
 
         $scope.productData = {
             selectedProductPrice: 0,
             currentProductPrice: 0,
             products: [
-                {'id':1, 'name':'Certificate', 'price':700},
-                {'id':2, 'name':'Conference', 'price':400},
-                {'id':3, 'name':'Workshop', 'price':3000}
+                {'id': 1, 'name': 'Certificate', 'price': 700},
+                {'id': 2, 'name': 'Conference', 'price': 400},
+                {'id': 3, 'name': 'Workshop', 'price': 3000}
             ]
         };
 
-        $scope.calculatePrice = function() {
-            if($scope.formData.voucherCode != undefined) {
+        $scope.calculatePrice = function () {
+            if ($scope.formData.voucherCode != undefined) {
                 Vouchers.validateVoucher($scope.formData.voucherCode)
-                    .success(function(voucher) {
+                    .success(function (voucher) {
                         $scope.productData.currentProductPrice = 666;
-                });
+                        $scope.errorMessage = null;
+                    })
+                    .error(function (voucher) {
+                        $scope.errorMessage = "Invalid voucher!";
+                    });
             }
         };
 
-        $scope.updatePrice = function() {
-          $scope.productData.currentProductPrice = $scope.productData.selectedProductPrice;
+        $scope.updatePrice = function () {
+            $scope.productData.currentProductPrice = $scope.productData.selectedProductPrice;
         };
 
-	});
+    });
