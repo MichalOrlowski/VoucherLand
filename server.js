d0fc63ddc4ebd776c8d546bf85edf69899912b6d
mior <michal.orlowski@jcommerce.pl>
2016-02-15 17:22:55 +0100

    Fixed deprecation warning from Express.

--- server.js
+++ server.js
@@ -16,7 +16,8 @@ mongoose.connect(database.url, function (error) {
 app.configure(function() {
 	app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
 	app.use(express.logger('dev')); 						// log every request to the console
-	app.use(express.bodyParser()); 							// pull information from html in POST
+	app.use(express.json());
+	app.use(express.urlencoded());							// pull information from html in POST
 	app.use(express.methodOverride()); 						// simulate DELETE and PUT
 });
 
